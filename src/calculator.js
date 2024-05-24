981ae01cab7cff50a7be330c9340f90f80661142
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2024-05-25 00:19:02 +0700

    create function to split bill

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,18 @@
 function calculateTax(totalBill, taxPercentage) {
     return totalBill * (taxPercentage / 100);
 }
+
+function splitBill(totalBill, itemsByPerson, taxPercentage) {
+    const totalTaxByPerson = itemsByPerson.map((items) =>
+        calculateTax(calculateTotal(items), taxPercentage)
+    );
+
+    return {
+        totalTaxByPerson,
+    };
+}
+
+module.exports = {
+    calculateTax,
+    splitBill,
+};
