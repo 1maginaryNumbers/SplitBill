b6bdf1660dc32f8fd73bcaee7fe6bb38c95488df
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2024-05-25 00:21:47 +0700

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const readline = require("readline");
+const { calculateTotal, calculateTax, splitBill } = require("./calculator");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Masukkan jumlah orang: ", (numberOfPeople) => {
+    const peopleItems = [];
+    const peopleNames = [];
+
+    askForItemsForPerson(
+        rl,
+        peopleNames,
+        peopleItems,
+        0,
+        numberOfPeople,
+        () => {
+            askTaxPercentage(rl, (taxPercentage) => {
+                const totalBill = peopleItems.reduce(
+                    (acc, items) => acc + calculateTotal(items),
+                    0
+                );
+                const {
+                    totalBillWithTax,
+                    totalBillByPerson,
+                    totalTaxByPerson,
+                    totalTax,
+                    subtotalByPerson,
+                    totalBillWithoutTax,
+                } = splitBill(
+                    totalBill,
+                    peopleItems,
+                    parseFloat(taxPercentage)
+                );
+                console.log(
+                    `Total tagihan tanpa pajak: ${totalBillWithoutTax}`
+                );
+                console.log(`Total pajak keseluruhan: ${totalTax}`);
+                console.log(`Total tagihan dengan pajak: ${totalBillWithTax}`);
+                console.log("Tagihan masing-masing orang:");
+                totalBillByPerson.forEach((bill, index) => {
+                    console.log(
+                        `${peopleNames[index]}: Total Tagihan: ${bill}, Pajak: ${totalTaxByPerson[index]}, Subtotal: ${subtotalByPerson[index]}`
+                    );
+                });
+            });
+        }
+    );
+});
