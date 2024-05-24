0284d12bbbb87835e0abdf33207ea54f2e24cd72
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2024-05-25 00:19:21 +0700

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -6,6 +6,9 @@ function splitBill(totalBill, itemsByPerson, taxPercentage) {
     const totalTaxByPerson = itemsByPerson.map((items) =>
         calculateTax(calculateTotal(items), taxPercentage)
     );
+    const totalBillByPerson = itemsByPerson.map(
+        (items, index) => calculateTotal(items) + totalTaxByPerson[index]
+    );
 
     return {
         totalTaxByPerson,
