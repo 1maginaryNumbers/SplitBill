8734feb8bfe7aa91f51ca7278cb93738d7b253b8
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2024-05-25 00:23:18 +0700

    Update prompts.js

--- src/prompts.js
+++ src/prompts.js
@@ -1,3 +1,50 @@
+function askForItemsForPerson(
+    rl,
+    peopleNames,
+    peopleItems,
+    personIndex,
+    numberOfPeople,
+    callback
+) {
+    rl.question(`Masukkan nama orang ke-${personIndex + 1}: `, (name) => {
+        peopleNames.push(name);
+        rl.question(`Masukkan jumlah item untuk ${name}: `, (numItems) => {
+            const items = [];
+            let currentItem = 1;
+
+            function askForItemPrice() {
+                rl.question(
+                    `Masukkan harga item ${currentItem} untuk ${name}: `,
+                    (price) => {
+                        items.push({ price: parseFloat(price) });
+                        currentItem++;
+
+                        if (currentItem <= numItems) {
+                            askForItemPrice();
+                        } else {
+                            peopleItems.push(items);
+                            if (personIndex + 1 < numberOfPeople) {
+                                askForItemsForPerson(
+                                    rl,
+                                    peopleNames,
+                                    peopleItems,
+                                    personIndex + 1,
+                                    numberOfPeople,
+                                    callback
+                                );
+                            } else {
+                                callback();
+                            }
+                        }
+                    }
+                );
+            }
+
+            askForItemPrice();
+        });
+    });
+}
+
 function askTaxPercentage(rl, callback) {
     rl.question("Masukkan persentase pajak: ", (taxPercentage) => {
         callback(taxPercentage);
@@ -5,5 +52,6 @@ function askTaxPercentage(rl, callback) {
 }
 
 module.exports = {
+    askForItemsForPerson,
     askTaxPercentage,
 };
